3ff8307a5d07180b4b48c076e4c086b4a93d7758
Edv <ederromero32@gmail.com>
2025-11-24 20:49:47 -0500

    usando ngModel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
+  userName: string = '';
   number: number = 0;
 
   addOne() {
-    this.number ++;
-
-    if (this.number > 10) {
-      this.number = 0;
-    }
+    this.number++;
   }
 
-  resetNumber() {
-    this.number = 0;
+  /* --- ADICIONAL ---
+  Con esta funcion podemos actualizar el contador de caracteres del nombre de usuario, aumenta y disminuye segun se escriba o borre texto.
+
+    updateCount() {
+    this.number = this.userName ? this.userName.length : 0;
   }
+  --- FIN ADICIONAL --- */
 
 }
